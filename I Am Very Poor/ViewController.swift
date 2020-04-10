6fe2d8d8f7c3040206d4b72ff6ae10a60fda9b8c
ohelwani <ohelwani@hemav.com>
2020-04-10 16:52:38 +0200

    Initial Commit

---
+++ I Am Very Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Very Poor
+//
+//  Created by Omar Helwani on 10/04/2020.
+//  Copyright © 2020 Omar Helwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
